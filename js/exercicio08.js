6f78b1e657fb4e47f819c53b354cb4c9c872905d
Rafael Leme <rafael.w.leme2603@gmail.com>
2026-08-25 22:03:58 -0300

    feat: gera tabuada utilizando for

---
+++ js/exercicio08.js
@@ -0,0 +1,11 @@
+ //declarando as variaveis 
+ 
+const numero = prompt ("Digite um numero que voce queira saber a tabuada !!!");
+
+
+//condicional para a tabuada 
+for (let i = 1; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
+
+console.log( "Oloko bichooo!!!");
