55a99e94d0ba37d965db32ad740fb9517ac94ef5
Rafael Leme <rafael.w.leme2603@gmail.com>
2026-08-25 21:36:54 -0300

    feat: implementa validação simples e login

---
+++ js/exercicio07.js
@@ -0,0 +1,14 @@
+//Declarando variaveis 
+const usuario = prompt ("Digite seu usuario")
+let senha = prompt ("Digite a sua senha")
+
+// condicionais do usuario 
+        if ( usuario === "admin" && senha === "1234") {
+            alert ("Acesso Permitido")
+        } else {
+            alert ("Acesso negado")
+        }
+
+
+// = é usado para atribuição, ou seja, para colocar um valor em uma variável.
+// === é usado para comparação estrita, verificando se o valor e o tipo são iguais.
